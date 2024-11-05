36d82800272b02ea5b20d7a6062fd39bd987baa7
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-11-05 13:48:41 +0100

    ajout de panel et de btn

--- CelluleMutante/Form1.Designer.cs
+++ CelluleMutante/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnl_cell = new System.Windows.Forms.Panel();
+            this.btn_simulation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnl_cell
+            // 
+            this.pnl_cell.Location = new System.Drawing.Point(193, 47);
+            this.pnl_cell.Name = "pnl_cell";
+            this.pnl_cell.Size = new System.Drawing.Size(423, 354);
+            this.pnl_cell.TabIndex = 0;
+            // 
+            // btn_simulation
+            // 
+            this.btn_simulation.Location = new System.Drawing.Point(193, 407);
+            this.btn_simulation.Name = "btn_simulation";
+            this.btn_simulation.Size = new System.Drawing.Size(423, 31);
+            this.btn_simulation.TabIndex = 0;
+            this.btn_simulation.Text = "Simulation";
+            this.btn_simulation.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_simulation);
+            this.Controls.Add(this.pnl_cell);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_cell;
+        private System.Windows.Forms.Button btn_simulation;
     }
 }
 
